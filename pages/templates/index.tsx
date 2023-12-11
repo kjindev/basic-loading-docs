093529a122bb875952ede9d36272dd4f7359f6fa
kjin <pkjin.eng@gmail.com>
2023-12-11 22:07:20 +0900

    feat: add new pages

---
+++ pages/templates/index.tsx
@@ -0,0 +1,17 @@
+import { css } from "@emotion/react";
+
+export default function Templates() {
+  return (
+    <div
+      css={css({
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      })}
+    >
+      Templates
+    </div>
+  );
+}
