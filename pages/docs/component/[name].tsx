3d8a74aa61d42e8701ec4696fe40c23b7234f93f
kjin <pkjin.eng@gmail.com>
2023-12-12 16:01:00 +0900

    feat: add docs dynamic route

--- pages/docs/component/[name].tsx
+++ pages/docs/component/[name].tsx
@@ -1,20 +1,87 @@
 import DocsLayout from "@/pages/_component/layout/DocsLayout";
+import CodeBlock from "@/pages/_component/pages/docs/CodeBlock";
+import OptionType from "@/pages/_component/pages/docs/OptionType";
+import Sample from "@/pages/_component/pages/docs/Sample";
+import { roboto_bold } from "@/pages/_util/font";
 import { css } from "@emotion/react";
+import { useParams } from "next/navigation";
 
 const DocsComponent = () => {
+  // const { name }: { name: string } = useParams();
+  const name = "Skeleton";
+
   return (
     <DocsLayout>
+      return (
       <div
         css={css({
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          "& .docs-container": {
+            padding: "80px 0 0 280px",
+          },
+          "& .docs-category": {
+            fontSize: "0.875rem",
+          },
+          "& .docs-title": {
+            fontSize: "2em",
+          },
+          "& .docs-content-container": {
+            margin: "2rem 0",
+          },
+          "& .code-block": {
+            margin: "0.75rem 0",
+          },
+          "& .option-type": {
+            display: "flex",
+            textAlign: "center",
+            fontSize: "0.875rem",
+            backgroundColor: "#00000010",
+            margin: "0.75rem 0",
+            padding: "0.75rem 0",
+            borderRadius: "0.75rem",
+            "& span": {
+              width: "25%",
+            },
+          },
+          "& .sample": {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "250px",
+            borderRadius: "0.75rem",
+            boxShadow:
+              "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);",
+            margin: "0.75rem 0",
+          },
         })}
       >
-        name
+        <div className="docs-container">
+          <div className="docs-category">Component</div>
+          <div className={`docs-title ${roboto_bold.className}`}>{name}</div>
+          <div className="docs-content-container">
+            <div className={`${roboto_bold.className}`}>• Sample</div>
+            <div className="sample">
+              <Sample title={name} />
+            </div>
+          </div>
+          <div className="docs-content-container">
+            <div className={`${roboto_bold.className}`}>• Code</div>
+            <div className="code-block">
+              <CodeBlock title={name} />
+            </div>
+          </div>
+          <div className="docs-content-container">
+            <div className={`${roboto_bold.className}`}>• Option Type</div>
+            <div className={`option-type ${roboto_bold.className}`}>
+              <span>name</span>
+              <span>type</span>
+              <span>default</span>
+              <span>unit</span>
+            </div>
+            <OptionType title={name} />
+          </div>
+        </div>
       </div>
+      );
     </DocsLayout>
   );
 };
